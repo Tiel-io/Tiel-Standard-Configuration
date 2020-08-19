e933883feb2147f3ed4c675e9961a1f8570823bc
Timothy Clancy <tim@rockhopper.us>
2020-08-19 04:29:35 -0400

    Moved improve screen locking to standalone script.

---
+++ usr/local/bin/screen-lock.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+## Lock the screen, update the desktop background, and select a new lock image.
+betterlockscreen -l blur & betterlockscreen -w & betterlockscreen -u ~/Pictures/Wallpapers/
