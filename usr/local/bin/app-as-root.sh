44791ade523518dac40ae683adc4ff7b427b22db
Timothy Clancy <tim@rockhopper.us>
2020-08-21 20:04:19 -0400

    Fixed password-asking script.

--- usr/local/bin/app-as-root.sh
+++ usr/local/bin/app-as-root.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## Use our Rofi password helper to ask for the sudo password.
-export SUDO_ASKPASS=$(/usr/local/bin/rofi-ask-password.sh "Sudo Password: ")
+export SUDO_ASKPASS=/usr/local/bin/rofi-ask-password.sh
 
 ## Execute the application; some applications require special logic for this.
 if [[ "$1" == "atom" ]]; then
