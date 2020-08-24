416e5f775a7155fc4472624b6e64053369f719c3
Timothy Clancy <tim@rockhopper.us>
2020-08-24 01:25:51 -0400

    Screen locking should not be as jarring to desktop.

--- tiel-standard-bootstrapper.sh
+++ tiel-standard-bootstrapper.sh
@@ -4,6 +4,12 @@
 ## @author Tim Clancy
 ## @date 8.17.2020
 
+export SUDO_ASKPASS=/usr/local/bin/rofi-ask-password.sh
+if [ ! -f $SUDO_ASKPASS ]; then
+  sudo ls > /dev/null
+else
+  sudo -A ls > /dev/null
+fi
 mkdir ~/.tiel-standard
 cd ~/.tiel-standard
 wget -O tiel-standard-linux-installer.sh https://raw.githubusercontent.com/Tiel-io/Tiel-Standard-Configuration/master/tiel-standard-linux-installer.sh
--- usr/local/bin/screen-lock.sh
+++ usr/local/bin/screen-lock.sh
@@ -6,7 +6,7 @@ source $MAIN_CONF
 
 ## If we are not supposed to cycle wallpapers, simply lock.
 if [ "$RANDOMIZE_DESKTOP" = false ]; then
-  betterlockscreen -l blur
+  betterlockscreen -l blur & betterlockscreen -w
 
 ## Lock the screen, update the desktop background, and select a new lock image.
 else
